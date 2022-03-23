510a5199a8fe106e9cfe52fa7149c99100149171
Alejandro Toledo <atoledofr@gmail.com>
2022-03-23 10:55:32 -0500

    add unit test for the searchbox component

---
+++ src/components/SearchBox.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import SearchBox from './SearchBox';
+
+describe('SearchBox', () => {
+  it('renders input', () => {
+    const props = {
+      term: '',
+      onSearch: jest.fn(),
+    };
+
+    const { container } = render(<SearchBox {...props} />);
+    const input = container.querySelector('input[type="text"]');
+    userEvent.type(input, 'domain');
+    expect(props.onSearch).toHaveBeenCalled();
+  });
+});
